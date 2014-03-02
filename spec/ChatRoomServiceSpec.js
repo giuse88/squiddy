ac9e02e7555e90e72a4b9a900fc00628681a1571
Giuseppe <giuse88@gmail.com>
2014-03-02 19:51:46 +0000

    Added 7 tests and exception to chatRoomService

--- spec/ChatRoomServiceSpec.js
+++ spec/ChatRoomServiceSpec.js
@@ -1,5 +1,4 @@
 var roomServiceUnderTest = require('../lib/ChatRoomService').getRoomService();
-SIZE_ID = 20;
 
 describe("Test Chat Room Service ", function() {
     "use strict";
@@ -23,6 +22,27 @@ describe("Test Chat Room Service ", function() {
       expect(roomServiceUnderTest.doesRoomExist("AnyRoom")).toBeFalsy();
     });
 
+    it("Test Deleting an empty room ", function() {
+        var roomId =  roomServiceUnderTest.createNewRoom();
+        expect(roomId).toBeDefined();
+        expect(roomId).not.toBeNull();
+        expect(roomServiceUnderTest.doesRoomExist(roomId)).toBeTruthy();
+        expect(roomServiceUnderTest.isRoomEmpty(roomId)).toBeTruthy();
+        roomServiceUnderTest.removeRoom(roomId);
+        expect(roomServiceUnderTest.doesRoomExist(roomId)).toBeFalsy();
+    });
+
+    it("Test that trying to delete a no existing room throws a RoomNotFoundException", function() {
+        try {
+           roomServiceUnderTest.removeRoom("noExistingRoom");
+        }catch (e){
+            expect(e.name).toEqual("RoomNotFoundException");
+        }
+    });
+
+    it("Test Adding peers to a room", function() {
+        expect(true).toBe(true);
+    });
     it("Test Adding peers to a room", function() {
         expect(true).toBe(true);
     });
