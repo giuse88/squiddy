fef7f2eb3158410526befa2d29f4977d5b516cc4
Giuseppe <giuse88@gmail.com>
2014-03-08 06:47:50 +0000

    Inserted log.js module

--- public/javascripts/log.js
+++ public/javascripts/log.js
@@ -1,18 +1,34 @@
-function() {
+(function() {
     "use strict";
+
+    var LOG =   "INFO ";
+    var TRACE = "TRACE"
+    var ERROR = "ERROR"
 
     function _log(type, msg ) {
         console.log("[" + type + "] " +  msg);
     }
 
-    function info(msg) {
-        _log(LOG, )
+    function info (msg) {
+        _log(LOG, msg );
     }
 
+    function error (msg) {
+        _log(ERROR, msg);
+    }
+
+    function trace (msg) {
+        _log(TRACE, msg);
+    }
+
+    // ===========================
+    //      PUBLIC INTERFACE
+    //============================
+
     return {
-        info();
-        error();
-        trace();
+        info  : info,
+        error : error,
+        trace : trace
     };
 
-}();
+})()
