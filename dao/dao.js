264cf225aaa703122c48469991b2a0c4a63e9ef7
Giuseppe <giuse88@gmail.com>
2014-04-09 22:12:57 +0100

    Fixed query rejected error.

--- dao/dao.js
+++ dao/dao.js
@@ -2,48 +2,24 @@
 
     var pg = require('pg');
     var connectionString = process.env.DATABASE_URL || 'postgres://giuseppe@localhost/jelly_db'
-    var client = new pg.Client(connectionString);
-    var connected = false;
-
-    var initialize = function(callBack, errorCallBack) {
-        client.connect(function(err) {
-            if(err) {
-                connected = false;
-                LOG.error("Dao initialization failed.", err);
-                errorCallBack && errorCallBack(err);
-            }else {
-                connected = true;
-                LOG.trace("Dao initialized.");
-                callBack && callBack();
-            }
-        });
-    };
-
-
-    var makeQuery = function(queryStr, callback, errorCallback) {
-        var query = client.query(queryStr);
-        query.on('err', errorCallback);
-        query.on('end', callback);
-    }
 
     var insert = function (queryStr, callback, errorCallback) {
-       if(!connected) {
-           initialize( function() {
-              makeQuery(queryStr, callback, errorCallback);
-           }, function(err) {
-            LOG.error("Error connection initialization.", err);
-           });
-       }
-        else
-          client.query(queryStr, callback, errorCallback);
+        pg.connect(connectionString, function(err, client, done) {
+            if(err) {
+                LOG.error('error fetching client from pool', err);
+            }
+            client.query(queryStr, '', function(err, result) {
+                //call `done()` to release the client back to the pool
+                done();
+                //
+                if(err)
+                    errorCallback && errorCallback(err);
+                else
+                   callback && callback(result);
+            });
+        });
     }
 
-    var release = function() {
-        client.end();
-        LOG.trace("Client released.");
-    };
-
-    exports.release = release;
     exports.insert = insert;
 
 })(exports,LOGGER);
