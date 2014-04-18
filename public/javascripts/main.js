978a16ce2c60a9ce55447e950cbd814b53cae3a1
Giuseppe <giuse88@gmail.com>
2014-04-18 15:25:13 +0200

    Added modal error modal dialog.

---
+++ public/javascripts/main.js
@@ -0,0 +1,23 @@
+var app = app || {};
+
+(function($) {
+
+    function renderBrowserNotSupportedError() {
+        $('#modal-unsupported-dialog').modal({
+            show : true,
+            backdrop: 'static',
+            keyboard: false
+        });
+    };
+
+    // public
+    function main() {
+        if ( navigator.sayswho.indexOf('Chrome') > -1 )
+            new app.AppView();
+        else
+            renderBrowserNotSupportedError();
+    }
+
+    app.main = main;
+
+})($);
